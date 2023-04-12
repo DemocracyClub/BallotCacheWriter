990c657dc0233d5616f93162e8917ada87fc86ae
Sym Roe <sym.roe@talusdesign.co.uk>
2023-04-12 15:37:20 +0100

    Don't call str

--- ballot_writer/write_ballots.py
+++ ballot_writer/write_ballots.py
@@ -98,7 +98,7 @@ class S3WriterBackend(BaseWriterBackend):
         )
 
     def get_latest_write_date(self):
-        last_updated_file = self.last_updated_path()
+        last_updated_file = self.last_updated_path
         try:
             return (
                 self.bucket.Object(last_updated_file).get()["Body"].read().decode()
